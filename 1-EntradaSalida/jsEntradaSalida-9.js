927bca9efbd29d60251872a7ca88796a361b7ffe
alanmurua <alan.murua@hotmail.com>
2019-02-08 21:00:18 -0300

    ejercicio9

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,18 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+    var num1
+    var num2
+    var aumento
+    var porcentaje
+    num1 = document.getElementById("sueldo").value
+    porcentaje = num1 * 0.1
+    aumento = parseInt(num1) + parseInt(porcentaje)
+    num2 = aumento
+    document.getElementById("resultado").value = num2
+      
+    
+    
 
-	
-}
+    }
+
