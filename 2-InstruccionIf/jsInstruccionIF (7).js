b888f35b51329901d599d1b1622844549b58c852
alanmurua <alanmurua@hotmail.com>
2019-02-13 20:02:11 -0300

    ejercicio2-7

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -1,6 +1,12 @@
 function mostrar()
 {
 //tomo la edad  
+var edad
+var estadoCivil
+edad = parseInt (document.getElementById("edad").value)
+estadoCivil = document.getElementById("estadoCivil").value
+if (edad <=17 && estadoCivil !== "Soltero")
+alert ("Es muy pequeño para NO ser soltero.")
 
 	
 
