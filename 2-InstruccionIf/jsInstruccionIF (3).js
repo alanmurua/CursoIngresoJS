69c552d9c7aae76bf2329e97b6a4956ce1254a2d
alanmurua <alanmurua@hotmail.com>
2019-02-13 19:13:03 -0300

    ejercicio 2-4

--- 2-InstruccionIf/jsInstruccionIF (3).js
+++ 2-InstruccionIf/jsInstruccionIF (3).js
@@ -1,6 +1,10 @@
 function mostrar()
 {
 //tomo la edad  
-
-
+var edad
+edad = parseInt (document.getElementById("edad").value)
+if (edad >= 18)
+alert ("mayor de edad")
+else
+alert("menor de edad")
 }//FIN DE LA FUNCIÓN
