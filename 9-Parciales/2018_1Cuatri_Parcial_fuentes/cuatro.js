aece2e9578588ef8cfe27cb961a8ae63010e3ee2
alanmurua <alanmurua@hotmail.com>
2019-02-13 21:59:31 -0300

    parcial4

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,27 @@
 function mostrar()
 {
+var num1
+var num2
+var resta
+var suma
+num1 =parseInt (prompt ("ingrese numero 1"))
+num2 =parseInt (prompt ("ingrese numero 2"))
+resta = num1 - num2
+suma = num1 + num2
+if (num1 == num2)
+alert ("los numeros " +num1+" y " +num2+" son iguales")
+else
+if (num1 > num2)
+alert ("la resta de " +num1+ " y " +num2+ " da como resultado "+ resta) 
+else (suma <= parseInt (10))
+alert("la suma de " +num1+ " y " +num2+ " da como resultado "+ suma)
+if (suma > parseInt (10))
+alert ("la suma es " +suma+  " y supero el 10")
+
+
+
+
+
+
 
 }
