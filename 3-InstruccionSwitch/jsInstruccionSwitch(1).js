81ee4be6da5e2a444e00edf61e78ea66a646ceec
alanmurua <alanmurua@hotmail.com>
2019-02-15 19:25:48 -0300

    recu1-2

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -3,7 +3,10 @@ function mostrar()
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
-
+switch (mesDelAño){
+case "Enero":
+document.write ("Feliz comienzo de año")
+break;
 
 
 }//FIN DE LA FUNCIÓN
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,14 @@
 
 function mostrar()
 {
+var alto
+var base
+var area
+var perimetro
+alto = parseInt (prompt ("alto de un rectangulo"))
+base = parseInt (prompt ("ancho de un rectangulo"))
+area = alto * base
+perimetro = (alto*2) + (base*2)
+alert ("el perimetro es de " +perimetro)
 
 }
