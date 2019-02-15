8b330227090f2467d0260f5b125678e6e50f41a7
alanmurua <alanmurua@hotmail.com>
2019-02-15 19:44:25 -0300

    ejercicio1

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -5,10 +5,10 @@ var mascota1
 var mascota2    
 var mascota1Kilos
 var mascota2Kilos
-mascota1Kilos = parseInt (15)
-mascota2Kilos = parseInt (10)
-mascota1 = "toby"
-mascota2 = "wanda"
+mascota1 = prompt ("ingrese nombre de su primer mascota")
+mascota2 = prompt ("ingrese nombre de su segunda mascota")
+mascota1Kilos = parseInt (prompt("ingrese los kilos de su primer mascota"))
+mascota2Kilos = parseInt (prompt("ingrese los kilos de su segunda mascota"))
 kilosTotales = mascota1Kilos + mascota2Kilos
 alert ("tenes dos mascotas "+mascota1+ " y "+mascota2+" que pesan "+mascota1Kilos+ " y "+mascota2Kilos+" kilos. La suma de los kilos es de "+kilosTotales)
 
