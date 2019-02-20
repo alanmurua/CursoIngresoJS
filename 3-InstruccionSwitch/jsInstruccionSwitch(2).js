66a1660f818762b7addf2c3963be16e3c4543de1
alumno <alumno>
2019-02-20 19:52:16 -0300

    switch
    Signed-off-by: alumno <alumno>

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -4,15 +4,14 @@ function mostrar()
 var mesDelAño = document.getElementById('mes').value;
 switch (mesDelAño){
 case "Enero":
-alert ("Falta para el invierno.")
 case "Febrero":
 case "Marzo":
 case "Abril":
 case "Mayo":
 case "Junio" :
+alert ("Falta para el invierno.")
 break;
 case "Julio":
-alert ("Abrigate que hace frio.")
 case "Agosto":
 alert ("Abrigate que hace frio")
 break;
