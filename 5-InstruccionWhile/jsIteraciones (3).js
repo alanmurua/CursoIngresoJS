9d80156b8564abed260964b3ad8898daf2f399d6
alumno <alumno>
2019-02-20 21:41:23 -0300

    while 3
    Signed-off-by: alumno <alumno>

--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -1,7 +1,11 @@
 function mostrar()
 {
-
+var cont = 2
 var clave = prompt("ingrese el número clave.");
-
-
+while (clave !="utn750"){
+    clave  = prompt("Error ingrese el número clave.")
+    cont = cont-1
+if (clave != "utn750" && cont == 0)
+break;
+}
 }//FIN DE LA FUNCIÓN
